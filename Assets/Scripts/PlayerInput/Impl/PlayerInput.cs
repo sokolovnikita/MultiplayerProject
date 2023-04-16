c0b75e86e94e49f130fdb6a4ca0b6a17d35637d3
sokolovnikita <130765957+sokolovnikita@users.noreply.github.com>
2023-04-16 16:36:43 +0300

    Add character general rotate behavior (#8)

--- Assets/Scripts/PlayerInput/Impl/PlayerInput.cs
+++ Assets/Scripts/PlayerInput/Impl/PlayerInput.cs
@@ -32,11 +32,16 @@ public class PlayerInput : MonoBehaviour
 
     private void Update()
     {
-        _moveDirection = _newInputSystem.Player.Move.ReadValue<Vector2>();
-        _rotateDirection = _newInputSystem.Player.Rotate.ReadValue<Vector2>();
+        ReadInput();
 
         _controllableObject.Move(_moveDirection);
         if (_rotateDirection != new Vector2(0,0))
             _controllableObject.Rotate(_rotateDirection);
+    }
+
+    private void ReadInput()
+    {
+        _moveDirection = _newInputSystem.Player.Move.ReadValue<Vector2>();
+        _rotateDirection = _newInputSystem.Player.Rotate.ReadValue<Vector2>();
     }
 }
