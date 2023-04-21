8a77a80f023d1b66630f8d94a624659ee7d1b7f8
sokolovnikita <130765957+sokolovnikita@users.noreply.github.com>
2023-04-21 10:31:47 +0300

    feat(coin-bar): add coin bar (#21)

---
+++ Assets/Scripts/Character/UI/CoinBar/ActualCoinBar.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActualCoinBar : MonoBehaviour
+{
+    [SerializeField] private CharacterBase _character;
+
+    private Image _coinBarImage;
+
+    private void Awake()
+    {
+        _coinBarImage = GetComponent<Image>();
+    }
+
+    private void OnEnable()
+    {
+        _character.OnTookCoinEvent += UpdateCoinBar;
+    }
+
+    private void OnDisable()
+    {
+        _character.OnTookCoinEvent -= UpdateCoinBar;
+    }
+
+    private void UpdateCoinBar(int maxCoins, int actualCoins)
+    {
+        Debug.Log($"actual - {actualCoins} max - {maxCoins}");
+        _coinBarImage.fillAmount = (float)actualCoins / (float)maxCoins;
+    }
+}
